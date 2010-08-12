1c98c71f20b9b1f4a34cad97e72ff1b9cbdf8a41
Ben Tillman <bstillman@crossroadsint.org>
2010-08-12 12:29:13 +0800

    Removed test folder fixed a couple of typos

--- features/support/plugin.rb
+++ features/support/plugin.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../lib/factories')
+require File.expand_path(File.dirname(__FILE__) + '/../../spec/factories')
 
 Before do
   ::ActiveRecord::Base.connection.execute('DROP TABLE IF EXISTS customfields_for_tag_1')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,25 @@
-begin
-  require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
-rescue LoadError
-  puts "You need to install rspec in your base app"
-  exit
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
+  #~ begin
+    require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
+  #~ rescue LoadError
+    #~ puts "You need to install rspec in your base app"
+    #~ exit
+  #~ end
+
+  require File.dirname(__FILE__) + "/factories.rb"
+
+  plugin_spec_dir = File.dirname(__FILE__)
+  ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
 end
 
-require File.dirname(__FILE__) + "/factories.rb"
+Spork.each_run do
+  # This code will be run each time you run your specs.
 
-plugin_spec_dir = File.dirname(__FILE__)
-ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
-
+end
