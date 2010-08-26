7c177caa677b395efdd6e005ee4745ef58f6127a
Nathan B <nathan.f77@gmail.com>
2010-08-26 16:27:15 +0800

    I like capitals

--- init.rb
+++ init.rb
@@ -6,7 +6,8 @@ FatFreeCRM::Plugin.register(:crm_super_tags, initializer) do
        version "0.1"
    description "Admin module and view hooks for super tags"
   dependencies :"acts-as-taggable-on", :haml, :simple_column_search
-           tab :admin, :text => "Super tags", :url => { :controller => "admin/super_tags" }
+           tab :admin, :text => "Super Tags", :url => { :controller => "admin/super_tags" }
 end
 
 require "crm_super_tags"
+
