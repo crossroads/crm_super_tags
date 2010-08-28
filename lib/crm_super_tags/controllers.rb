869bf95b413c6751d1c7f340418100982335d9d2
Nathan B <nathan.f77@gmail.com>
2010-08-28 10:46:29 +0800

    Made supertag customfields reset when a supertag is removed from the input

--- lib/crm_super_tags/controllers.rb
+++ lib/crm_super_tags/controllers.rb
@@ -5,11 +5,9 @@
       if params[:tags]
         tags = params[:tags].split(",")
         @super_tags = tags.map {|t| ActsAsTaggableOn::Tag.find_by_name(t.strip) }.compact.uniq
-        unless @super_tags.empty?
-          model_klass = self.class.name[/(.*)Controller/, 1].singularize.constantize
-          @asset = model_klass.find_by_id(params[:asset_id]) || model_klass.new
-          render :template => "common/super_tags.js" and return
-        end
+        model_klass = self.class.name[/(.*)Controller/, 1].singularize.constantize
+        @asset = model_klass.find_by_id(params[:asset_id]) || model_klass.new
+        render :template => "common/super_tags.js" and return
       end
       # Else, render nothing if no supertags were found.
       render :text => ""
