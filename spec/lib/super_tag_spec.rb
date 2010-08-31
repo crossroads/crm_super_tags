51b198f2628576bf23812a0686d85c3983d87f38
Nathan B <nathan.f77@gmail.com>
2010-08-31 12:21:59 +0800

    Fixed specs

--- spec/lib/super_tag_spec.rb
+++ spec/lib/super_tag_spec.rb
@@ -1,7 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SuperTag do
-  before(:each) { SuperTag::Clean.drop_tables }
+  before(:each) do
+    # Tests pass perfectly without dropping the tables between each step...
+    # SuperTag::Clean.drop_tables
+  end
 
   it "should define tag1" do
     Opportunity.new.tag1.should be_nil
