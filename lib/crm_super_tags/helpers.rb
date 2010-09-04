435ccd50708b4ef6ec37f088eb3b5184622270ee
Nathan B <nathan.f77@gmail.com>
2010-09-04 18:18:25 +0800

    'link_to_inline' helper method override does a check to make sure the javascript function extension is loaded first

--- lib/crm_super_tags/helpers.rb
+++ lib/crm_super_tags/helpers.rb
@@ -7,7 +7,7 @@ ApplicationHelper.module_eval do
     related = (options[:related] ? ", related: '#{options[:related]}'" : "")
 
     # Close any open 'edit' forms before loading the create form.
-    before_hook = id.to_s.start_with?("create_") ? "crm.close_all_forms();" : ""
+    before_hook = id.to_s.start_with?("create_") ? "if(crm.close_all_forms) crm.close_all_forms();" : ""
 
     link_to_remote(text,
       :url    => url,
