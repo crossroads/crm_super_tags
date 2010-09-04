c9518f6293afff56790a250ded0520e6416ef816
Nathan B <nathan.f77@gmail.com>
2010-09-04 15:07:48 +0800

    Now uses a global hash to store which forms have been loaded, and can dynamically add or remove single forms depending on tag actions

--- public/javascripts/crm_supertags.js
+++ public/javascripts/crm_supertags.js
@@ -1,11 +1,22 @@
 crm.set_tag_list_event = function(controller, asset, asset_id) {
   // Adds the 'on_change' hook for the FacebookList, to AJAX load supertag form fields.
-  var supertag_list_options = $H({
-      onChange: function(){
-        crm.load_supertag_fields(controller, $('tag_list').value, asset_id);
+  var extra_supertag_options = $H({
+      onAdd: function(tag){
+        // load the supertag form fields if not already loaded.
+        if(!loadedSupertagForms.get(tag)){
+          crm.load_supertag_fields(controller, tag, asset_id);
+        };
+      },
+      onDispose: function(tag){
+        // remove the supertag form fields if they were loaded.
+        var form_id = loadedSupertagForms.get(tag);
+        if(form_id){
+          $(form_id).remove();
+          loadedSupertagForms.unset(tag);
+        };
       }
   });
-  fbtaglist.options.update(supertag_list_options);
+  fbtaglist.options.update(extra_supertag_options);
 };
 
 crm.load_supertag_fields = function(controller, tags, asset_id) {
@@ -19,4 +30,7 @@ crm.load_supertag_fields = function(controller, tags, asset_id) {
                   collapsed : "no" }
   });
 };
+
+// {'supertag' => 'div element id'}
+var loadedSupertagForms = new Hash();
 
