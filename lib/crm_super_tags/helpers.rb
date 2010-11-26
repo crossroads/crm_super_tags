5dd353ef328051587a2133ef9fe8b47c1fb05210
Nathan B <nathan.f77@gmail.com>
2010-11-26 16:06:34 +0800

    Fixed html_safe string

--- lib/crm_super_tags/helpers.rb
+++ lib/crm_super_tags/helpers.rb
@@ -9,7 +9,7 @@ ApplicationHelper.module_eval do
     related = (options[:related] ? ", related: '#{options[:related]}'" : "")
 
     # Close any open 'edit' forms before loading the create form.
-    before_hook = id.to_s.start_with?("create_") ? "if(crm.close_all_forms && $('#{id}_arrow').innerHTML != '▼') crm.close_all_forms()" : ""
+    before_hook = id.to_s.start_with?("create_") ? "if(crm.close_all_forms && $('#{id}_arrow').innerHTML != '▼'){ crm.close_all_forms() }".html_safe : ""
 
     link_to_remote(text,
       {:url    => url,
