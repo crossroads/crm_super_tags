feeafb327a5bcfc1e020bb60065542fb2043d757
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-09 16:49:10 +0800

    Fixed defect #5609 - TypeError: can't dump anonymous class Class

--- lib/crm_super_tags/controllers.rb
+++ lib/crm_super_tags/controllers.rb
@@ -6,7 +6,7 @@
         model_klass = self.class.name[/(.*)Controller/, 1].singularize.constantize
         tags = params[:tags].split(",")
         @super_tags = tags.map do |t|
-          ActsAsTaggableOn::Tag.where(:name => t.strip).where('taggable_type IS NULL OR taggable_type = ?', model_klass).first
+          ActsAsTaggableOn::Tag.where(:name => t.strip).where('taggable_type IS NULL OR taggable_type = ?', model_klass.name).first
         end.compact.uniq
         @asset = model_klass.find_by_id(params[:asset_id]) || model_klass.new
         render :template => "common/super_tags.js" and return
